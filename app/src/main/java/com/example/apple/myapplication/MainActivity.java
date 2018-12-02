748b4d985cc6daa3fd23f4963587787cc37a1cf7
IvanZzz <ivan.nzhong@gmail.com>
2018-12-01 19:07:49 -0600

    way to go

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.apple.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Request queue for our network requests. */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set up a queue for our Volley requests
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
     }
 }
