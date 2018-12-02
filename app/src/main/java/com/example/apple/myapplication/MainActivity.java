abd831e16dbe0f191b04183d27ca2e363969e6b6
IvanZzz <ivan.nzhong@gmail.com>
2018-12-01 23:29:53 -0600

    way to go

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -51,14 +51,52 @@ public class MainActivity extends AppCompatActivity {
 
         //set submit button
         submitButton = findViewById(R.id.submitButton);
+
+        //Set the text view of flight departure information
+        final TextView flightDepartInfo = findViewById(R.id.flightDepartInfo);
+
+        //Set the departure information bar to invisible first
+        flightDepartInfo.setVisibility(View.INVISIBLE);
         //set input to flight number variable by using a listener
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 flightNumber = flightNumberInput.getText().toString();
                 Log.d(TAG, "Submit button clicked");
+                startAPICall(flightDepartInfo);
+                //Set the departure information bar to visible
+                flightDepartInfo.setVisibility(View.VISIBLE);
             }
         });
+
+    }
+
+    /**
+     * Make an API call.
+     */
+    void startAPICall(final TextView textView) {
+        try {
+            //final String ret;
+            JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                    Request.Method.GET,
+                    "https://api.iextrading.com/1.0/stock/AAPL/batch?types=quote",
+                    null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(final JSONArray response) {
+                            Log.d(TAG, response.toString());
+                            textView.setText(response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
