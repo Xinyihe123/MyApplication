47ef603cdafd289fc00e31cfea6f46938fbfe352
IvanZzz <ivan.nzhong@gmail.com>
2018-12-04 01:55:54 -0600

    way to go

--- app/src/main/java/com/example/apple/myapplication/SearchMenu.java
+++ app/src/main/java/com/example/apple/myapplication/SearchMenu.java
@@ -119,6 +119,7 @@ public class SearchMenu extends AppCompatActivity {
         returnHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
             }
         });
     }
