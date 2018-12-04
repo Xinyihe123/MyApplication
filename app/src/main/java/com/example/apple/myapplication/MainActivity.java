67214a5c77a833bea77cc5ceba88f6530b7679b7
IvanZzz <ivan.nzhong@gmail.com>
2018-12-04 01:53:39 -0600

    way to go

--- app/src/main/java/com/example/apple/myapplication/MainActivity.java
+++ app/src/main/java/com/example/apple/myapplication/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.apple.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,11 +17,6 @@ public class MainActivity extends AppCompatActivity {
     /** Default logging tag for messages from the main activity. */
     private static final String TAG = "Lab12:Main";
 
-    String flightNumber;
-
-    EditText flightNumberInput;
-
-    Button submitButton;
 
     /** Request queue for our network requests. */
     private static RequestQueue requestQueue;
@@ -45,68 +31,19 @@ public class MainActivity extends AppCompatActivity {
         // Load the main layout for our activity
         setContentView(R.layout.activity_main);
 
-        //set user input to flightNumberInput variable
-        flightNumberInput = findViewById(R.id.flightNumberInput);
+        configureSearchDepartArr();
+    }
 
-        //set submit button
-        submitButton = findViewById(R.id.submitButton);
-
-        //Set the text view of flight departure information
-        final TextView flightDepartInfo = findViewById(R.id.flightDepartInfo);
-
-        //set input to flight number variable by using a listener
-        submitButton.setOnClickListener(new View.OnClickListener() {
+    private void configureSearchDepartArr() {
+        Button searchDepartArriv = findViewById(R.id.searchDeparetureArrival);
+        searchDepartArriv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                flightNumber = flightNumberInput.getText().toString();
-                Log.d(TAG, "Submit button clicked");
-                //JSONObject arrival = null;
-                startAPICall(flightDepartInfo);
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SearchMenu.class));
             }
         });
-
     }
 
 
 
-    /**
-     * Make an API call.
-     */
-    void startAPICall(final TextView flightDepartInfo) {
-        try {
-            //final String ret;
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    //"https://aviation-edge.com/v2/public/flights?key=[36b1a4-a52bb5]&flightIata=" + flightNumber
-                    //"https://universities.hipolabs.com/search?name=" + flightNumber
-                    "https://aviation-edge.com/v2/public/flights?key=36b1a4-a52bb5&flightIata=" + flightNumber,
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            Log.d(TAG, response.toString());
-                            try {
-                                JSONArray arr = new JSONArray(response);
-                                //get departure info -- icaoCode
-                                JSONObject departure = arr.getJSONObject(2);
-                                String icaoCode = departure.getString("icaoCode");
-                                flightDepartInfo.setText(icaoCode);
-
-                            } catch (Exception e) {
-                                Log.e("MYAPP", "unexpected JSON exception", e);
-                            }
-
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    Log.w(TAG, error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
 }
